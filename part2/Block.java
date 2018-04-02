3a538469a569a40b6a2b11c0151c62680f31cdeb
Gaurav Anand <gv.anand.1997@gmail.com>
2018-04-02 19:48:11 +0530

    updated everything

--- part2/Block.java
+++ part2/Block.java
@@ -3,24 +3,25 @@ import java.util.Date;
 
 public class Block {
 	
-	public static String hash;
-	public static String previousHash; 
-	public static int proof_data; //our data will be a simple message.
-	public static long timeStamp; //as number of milliseconds since 1/1/1970.
-	public static int nonce;
+	public  String hash;
+	public  String previousHash; 
+	public  int proof_data; //our data will be a simple message.
+	public  long timeStamp; //as number of milliseconds since 1/1/1970.
+	public  int nonce;
 	
 	
 	//Block Constructor.  
 	public Block(String previousHash,int data) {
-		this.proof_data = data;
+//		this.proof_data = ;
 		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
 		
 		this.hash = calculateHash(); //Making sure we do this after we set the other values.
+		proof_of_work(3);
 	}
 	
 	//Calculate new hash based on blocks contents
-	public static String calculateHash() {
+	public  String calculateHash() {
 		String calculatedhash = StringUtil.applySha256( 
 				previousHash +
 				Long.toString(timeStamp) +
@@ -29,7 +30,7 @@ public class Block {
 				);
 		return calculatedhash;
 	}
-	public static String calculate_block_hash(Block block) {
+	public  String calculate_block_hash(Block block) {
 		String calculatedhash = StringUtil.applySha256( 
 				block.previousHash +
 				Long.toString(block.timeStamp) +
@@ -48,7 +49,7 @@ public class Block {
 	}
 
 
-	public static int proof_of_work(int difficulty) {
+	public  int proof_of_work(int difficulty) {
 		String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0" 
 		while(!hash.substring( 0, difficulty).equals(target)) {
 			nonce ++;
