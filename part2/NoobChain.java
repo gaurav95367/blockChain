7388cd8df3ebd3ae4bbd0d6fae046b3ee46f8188
Gaurav Anand <gv.anand.1997@gmail.com>
2018-04-02 19:47:43 +0530

    Update NoobChain.java

--- part2/NoobChain.java
+++ part2/NoobChain.java
@@ -10,19 +10,20 @@ import com.sun.net.httpserver.*;
 
 public class NoobChain {
 	
-	public static List<Block> blockchain = new ArrayList<Block>();
+	public static ArrayList<Block> blockchain = new ArrayList<Block>();
 	public static int difficulty = 5;
 	static int port = 8080;
 
 	// previous hash = 100 , proof data =1 for genesis block we can set anything
 
 	private static void create_genesis_block(){
-		Block genesis_block= new Block("100",1);
+		Block genesis_block= new Block("100",99);
 		blockchain.add(genesis_block);
 	}
 	// @Override
-	public static Block create_new_block(int proof, String previousHash){
-		Block new_block= new Block(previousHash,proof);
+	public static Block create_new_block(String previousHash){
+		Block new_block= new Block(previousHash,99);
+
 		blockchain.add(new_block);
 		return new_block;
 	}
@@ -42,17 +43,60 @@ public class NoobChain {
     	public void handle(HttpExchange t) throws IOException {
     		int len=blockchain.size();
             Block last_block= blockchain.get(len-1);
-            int proof=Block.proof_of_work(4);
+            //int proof=Block.proof_of_work(4);
 
             // this is too be done next . hash calculation and adding into chain
-            String previousHash=Block.calculate_block_hash(last_block);
-            Block new_block=create_new_block(proof,previousHash);
+            String previousHash=last_block.hash;
+           System.out.println("-----------------------------------");
+          	len=blockchain.size();
+            for (int i=0;i<len ;i++ ){
+            	String response="";
+            	// System.out.println("Index = "+i);
+            	// System.out.println("hash = "+);
+            	// System.out.println("Index = "+i);
+            	// System.out.println("Index = "+i);
+
+            	response=response+ "Index: " +(i+1) +"\n";
+            	response=response+ " previousHash: " +blockchain.get(i).previousHash +"\n";
+            	response=response+ "proof: " +blockchain.get(i).nonce+"\n" ;
+            	response=response+ "timestamp: " +blockchain.get(i).timeStamp+"\n";
+            	System.out.println(response);
+            }
+
+           System.out.println("---After adding");
+
+
+            Block new_block=create_new_block(previousHash);
+
+          	len=blockchain.size();
+            for (int i=0;i<len ;i++ ){
+            	String response="";
+            	// System.out.println("Index = "+i);
+            	// System.out.println("hash = "+);
+            	// System.out.println("Index = "+i);
+            	// System.out.println("Index = "+i);
+
+            	response=response+ "Index: " +(i+1) +"\n";
+            	response=response+ " previousHash: " +blockchain.get(i).previousHash +"\n";
+            	response=response+ "proof: " +blockchain.get(i).nonce+"\n" ;
+            	response=response+ "timestamp: " +blockchain.get(i).timeStamp+"\n";
+            	System.out.println(response);
+            }
+
+
+
+          
             String response="";
             response=response+ "<p> Index: " +(blockchain.size()) + "</p>";
             response=response+ "<p> previousHash: " +new_block.previousHash + "</p>";
-            response=response+ "<p> proof: " +new_block.proof_data + "</p>";
+            response=response+ "<p> proof: " +new_block.nonce + "</p>";
             response=response+ "<p> timestamp: " +new_block.timeStamp + "</p>";
             System.out.println(response);
+
+
+
+
+
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
@@ -68,7 +112,7 @@ public class NoobChain {
             	// response="";
             	response=response+ "<p> Index: " +(i+1) + "</p>";
             	response=response+ "<p> previousHash: " +blockchain.get(i).previousHash + "</p>";
-            	response=response+ "<p> proof: " +blockchain.get(i).proof_data + "</p>";
+            	response=response+ "<p> proof: " +blockchain.get(i).nonce + "</p>";
             	response=response+ "<p> timestamp: " +blockchain.get(i).timeStamp + "</p>";
             	// System.out.println(response);
             }
